486fb32c3bc18f0d179a42d22516dc1abbb83deb
oldu73 <oldu73@gmail.com>
2017-01-30 23:16:59 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,152 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class Controller {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    private File file;
+
+    public void initialize() {
+
+        System.out.println("Controller initialize..");
+
+        button.setOnAction(event -> doBrowse2());
+
+    }
+
+    private void doBrowse1() {
+
+        // SRC: http://stackoverflow.com/questions/37393642/creating-a-javafx-dialog-inside-a-javafx-task
+        Task<Void> browse = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        FileChooser fileChooser = new FileChooser();
+                        fileChooser.setTitle("Open File");
+
+                        File file = fileChooser.showOpenDialog(Main.primaryStage);
+
+                        label.setText(file.getName());
+
+                    }
+                });
+
+                return null;
+            }
+        };
+
+        // SRC: http://stackoverflow.com/questions/13838089/file-chooser-dialog-not-closing
+        Thread th = new Thread(browse);
+        th.setDaemon(true);
+        th.start();
+
+    }
+
+    private void doBrowse2() {
+
+        Scene scene = Main.scene;
+
+        final Cursor oldCursor = scene.getCursor();
+        scene.setCursor(Cursor.WAIT);
+
+        System.out.println("1");
+
+        //calculateItem.setDisable(true);
+        //calculateButton.setDisable(true);
+
+        final Service<Void> browseService = new Service<Void>() {
+
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+
+                    @Override
+                    protected Void call() throws Exception {
+
+                        Platform.runLater(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                System.out.println("2");
+
+                                FileChooser fileChooser = new FileChooser();
+                                fileChooser.setTitle("Open File");
+
+                                File file = null;
+
+                                while (file == null) {
+                                    file = fileChooser.showOpenDialog(Main.primaryStage);
+                                }
+                                
+                                final int maxIterations = 1000000;
+                                for (int iterations = 0; iterations < maxIterations; iterations ++) {
+                                    System.out.println(iterations);
+                                }
+
+                                label.setText(file.getName());
+
+                                System.out.println("3");
+
+                            }
+                        });
+
+                        /*
+                        final int maxIterations = 1000000;
+                        for (int iterations = 0; iterations < maxIterations; iterations ++) {
+                            System.out.println(iterations);
+                        }
+                        */
+
+                        return null;
+                    }
+                };
+            }
+        };
+
+        browseService.stateProperty().addListener((ObservableValue<? extends Worker.State> observableValue, Worker.State oldValue, Worker.State newValue) -> {
+            switch (newValue) {
+                case FAILED:
+                case CANCELLED:
+                case SUCCEEDED:
+                    scene.setCursor(oldCursor);
+
+                    System.out.println("4");
+
+                    //calculateItem.setDisable(false);
+                    //calculateButton.setDisable(false);
+
+                    break;
+            }
+        });
+
+        browseService.start();
+
+    }
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static Stage primaryStage;   // Static reference to primaryStage
+
+    public static Scene scene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.primaryStage = primaryStage;
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+
+        scene = new Scene(root, 300, 275);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
